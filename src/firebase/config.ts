161415818fb75a3ca0618dc13a779e8beb33baf3
周磊 <obiqeqop737@gmail.com>
2026-02-26 11:53:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-937136369-19c90",
+  "appId": "1:597923930898:web:6551ab868e5cf4f028c9cb",
+  "apiKey": "AIzaSyBpj7RiAJGFSWdmZAEFrPxvmLGfimoHQO0",
+  "authDomain": "studio-937136369-19c90.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "597923930898"
+};
